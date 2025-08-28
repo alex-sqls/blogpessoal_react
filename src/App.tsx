1b39c60efff9e040b06145e4cecc11d7441358f6
alex-sqls <alex-sql@outlook.com>
2025-08-27 21:42:58 -0300

    iniciando o front-end do blog pessoal

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './assets/pages/home/Home'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    //isso nao e html, e sim uma representacao do html
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
