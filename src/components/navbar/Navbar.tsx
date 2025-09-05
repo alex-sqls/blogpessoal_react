0fb43c71c9bc804b5716bf04eabb5e64bc3b6530
alex-sqls <alex-sql@outlook.com>
2025-09-05 18:12:17 -0300

    feat: adicionando o requisito deletar postagem

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -22,8 +22,8 @@ function logout() {
           Blog Pessoal
         </Link>
         <div className="flex gap-4">
-           <Link to="/teste">teste</Link>
-          <Link to="/postagens">Postagens</Link>
+              
+          <Link to='/postagens' className='hover:underline'>Postagens</Link>
           <Link to='/temas' className='hover:underline'>Temas</Link>
           <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
           <Link to="/perfil">Perfil</Link>
